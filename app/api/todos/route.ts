4603a0c5ad0ffc149b46c7259bb12eff477f63d8
Kathie Huang <kathie.huang@duke.edu>
2023-07-09 03:26:47 -0700

    incorporated planetscale and prisma, get and api endpoints are working on both backend and frontend

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,37 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
 
-let todos = [{ id: 1, text: "wash dishes", done: false }]
+// let todos = [{ id: 1, text: "wash dishes", done: false }]
+
+const prisma = new PrismaClient()
 
 export async function GET() {
+    const todos = await prisma.todo.findMany();
     return NextResponse.json(todos);
 }
 
 export async function POST(req: NextRequest) {
     const { text } = await req.json() // gets text property from JSON payload of request
-    const newTodo = { id: Date.now(), text, done: false };
-    todos.push(newTodo);
-    console.log(todos)
+    const newTodo = await prisma.todo.create({
+        data: {
+            text,
+            done: false
+        }
+    })
     return NextResponse.json({ newTodo })
 }
 
-export async function DELETE(req: NextRequest) {
-    const { searchParams } = new URL(req.url)
-    const id = searchParams.get('id')
-    if (id === null) {
-        return NextResponse.json({ error: 'Could not find todo to delete' }, { status: 400 });
-    }
-    console.log("in backend id")
-    console.log(id)
-    todos = todos.filter((todo) => todo.id !== +id)
-    return NextResponse.json({ id });
-}
+// export async function DELETE(req: NextRequest) {
+//     const { searchParams } = new URL(req.url)
+//     const id = searchParams.get('id')
+//     if (id === null) {
+//         return NextResponse.json({ error: 'Could not find todo to delete' }, { status: 400 });
+//     }
+//     console.log("in backend id")
+//     console.log(id)
+//     todos = todos.filter((todo) => todo.id !== +id)
+//     return NextResponse.json({ id });
+// }
 
-export async function PATCH(req: NextRequest) {
-    const { searchParams } = new URL(req.url)
-    const id = searchParams.get('id')
-    if (id === null) {
-        return NextResponse.json({ error: 'Could not find todo' }, { status: 400 });
-    }
-    todos = todos.map((todo) => (todo.id === +id ? { ...todo, done: !todo.done } : todo))
-    return NextResponse.json({ id })
-}
+// export async function PATCH(req: NextRequest) {
+//     const { searchParams } = new URL(req.url)
+//     const id = searchParams.get('id')
+//     if (id === null) {
+//         return NextResponse.json({ error: 'Could not find todo' }, { status: 400 });
+//     }
+//     todos = todos.map((todo) => (todo.id === +id ? { ...todo, done: !todo.done } : todo))
+//     return NextResponse.json({ id })
+// }
